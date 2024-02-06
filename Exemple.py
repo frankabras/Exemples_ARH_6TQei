0db7059be8594c5613ff8eeb619b666001102c4c
frankabras <frankabras@users.noreply.github.com>
2024-02-06 20:49:23 +0100

    Commentaires ajoutées

--- Exemple.py
+++ Exemple.py
@@ -1,29 +1,50 @@
 #Importation des librairies utilisées
-import utime
-import machine
 """
-import machine
+Les différentes manières d'importer une librairie :
+    import machine
     -> LED = machine.Pin(25,machine.Pin.OUT)
-from machine import Pin,PWM,ADC
+    from machine import Pin,PWM,ADC
     -> LED = Pin(25,Pin.OUT)
-from machine import *
-    -> Idem que précédent mais importe tous
-        les modules de la librairie
+    from machine import *
+    -> Idem que précédent mais importe tous les modules de la librairie
 """
+import machine
+"""
+La librairie "machine" contient :
+    -> Les fonctions d'entrées/sorties (Pin,PWM,ADC,...)
+    -> Les fonctions de communications (UART, I3C,...)
+    -> Les fonctions timers du microcontrôleur (Timer)
+"""
+
 
 #Définition des entrées/sorties
-BUTTON = machine.Pin(16,machine.Pin.IN)
-LED = machine.Pin(25,machine.Pin.OUT)
+BUTTON = machine.Pin(16,machine.Pin.IN)	#On définit la pin 16 comme 
+LED = machine.Pin(25,machine.Pin.OUT) 	#On définit la pin 25 comme sortie
+#La pin 25 du Raspberry Pico est connectée à une led intégrée sur la carte
 
 #Initialisation des variables 
-bp_val = 0
+bp_val = 0	#Variable utilisée pour stocker l'état de l'entrée
+"""
+Micro(Python) ne nécessite pas de spécifier le type de la variable (int,float,char,...) :
+    -> maVariable = 12				=> Entier (int)
+    -> maVariable = 1.2				=> Réel (float)
+    -> maVariable = 'a'				=> Caractère (char)
+    -> maVariable = "Hello World"	=> Chaine de caractères (char[n])
+L'attribution du type se fait automatiquement par l'interpreteur
+"""
 
-print("Hello World")
-while True:  
-    bp_val = BUTTON.value()
-    if bp_val == 1:
-        LED.on()
-        #LED.value(1)
-    else :
-        LED.off()
-        #LED.value(0)
+print("Hello World")	#Permet d'afficher un message dans le terminal
+"""
+Autres possibilité avec la fonction print :
+
+"""
+while True: 				#Comparable au while(1) {...} en langage C 
+    bp_val = BUTTON.value() #La fonction .value() pour récupérer l'état d'une entrée
+    if bp_val == 1:			#Vérifie si l'entrée est active
+        LED.on()			#La fonction .on() permet d'activer une sortie
+        #LED.value(1)		 #Autre méthode pour activer une sortie
+        #LED.value(True)	 #On peut également utiliser True
+    else :					#Dans le cas ou l'entrée est incative
+        LED.off()			#La fonction .off() permet de désactiver une sortie
+        #LED.value(0)		 #Autre méthode pour désactiver une sortie
+        #LED.value(True)	 #On peut également utiliser False
--- Exemple_v2.py
+++ Exemple_v2.py
@@ -1,23 +1,71 @@
 #Importation des librairies utilisées
-import utime
+import utime #Permet d'utiliser les foonctions tempo de MicroPython
 from machine import Pin,PWM,ADC
 
-#Déclaration de la fonction du BP
-def fonction_interruption(BUTTON):
+#Déclaration de la fonction d'interruption du BP
+"""
+Une interruption est un mécanisme qui bloque le programme afin d'exectuer
+un morceau de code lors d'un évènement particulier et qui reprend ensuite
+le code à l'endroit où il s'était arreté
+
+Dans ce cas-ci :
+Le programme s'arrête lorsqu'on appuie sur un BP, pour executer la fonction
+ci-dessous qui incrémente une variable et s'assure que sa valeur reste entre
+0 et 4
+
+Déclaration d'un fonction d'interruption :
+    def nom_fonction(entrée1, entrée2,...,entréeN) :
+        .
+        .
+        code de la fonction
+        .
+        .
+        return sortie #permet de renvoyer une variable au programme principal
+
+Utilisation de la fonction dans le code :
+    sortie = nom_fonction(entrée1, entrée2,...,entréeN)
+    
+Dans le cas d'une inturreption, il n'est pas nécessaire d'utilisation la
+fonction dans le code principale car elle est automatiquement appelée
+lorsqu'on appuie sur le bouton poussoir
+
+Par contre, il est ABSOLUMENT nécessaire d'indiquer le nom de l'entrée qui est
+utilisée pour l'interruption, comme entrée de la fonction (*)
+"""
+def fonction_interruption(BUTTON): #*
     global state
     state = state + 1
-    if state > 4:
+    if state == 5:
         state = 0
 
 #Définition des entrées/sorties
-BUTTON = Pin(16,Pin.IN, pull=Pin.PULL_UP)
+BUTTON = Pin(16,Pin.IN, pull=Pin.PULL_UP)	#On peut choisir une résistance de rappel
 BUTTON.irq(trigger=Pin.IRQ_FALLING,handler=fonction_interruption)
+"""
+La ligne ci-dessus permet de lier l'interruption au BP et à la fonction à executer :
+    trigger: Permet de choisir si l'interruption a lieue sur le front montant (passage 0->1)
+             ou sur le front descendant (passage 1->0)
+             - IRQ_RISING = front montant
+             - IRQ_FALLING = front descendant
+    handler: Permet de choisir la fonction à executer
+"""
 
 LED = Pin(25,Pin.OUT)
 
-LED_PWM = PWM(Pin(15))
-LED_PWM.freq(1_000)
+"""
+Les sorties PWM (Pulses Width Modulation - Modulation de largeur d'impulsions) permettent
+d'appliquer une tension variable à un éléments placer en sortie.
+(Exemple d'utilisation : faire varier la luminosité d'une led)
+"""
+LED_PWM = PWM(Pin(17))
+LED_PWM.freq(1_000) #Fréquence du signal PWM (plus spécifique)
 
+"""
+Les ADC (Analog to Digital Converter - Convertisseur Analogique-Digital) permmettent de
+convertir une valeur analogique qu'on lui transmet en entrée et de la convertir en valeur
+numérique utilisable par le microcontrôleur dans le programme
+(Exemple d'utilisation : lire la valeur d'un potentiomètre)
+"""
 POT = ADC(0)
 
 #Initialisation des variables 
@@ -43,15 +91,17 @@ while True:
     elif state == 4:
         LED.value(0)
         pot_val = POT.read_u16()
+        #Permet de récupérer la valeur convertie par l'ADC 
         LED_PWM.duty_u16(pot_val)
+        #Permet d'écrire la valeur PWM désirée (0 -> 65535 car 16bits)
+        """
+        Quelques valeurs d'exemple:
+            -> PWM.duty_u16(0) 		= 0/65535 = 0 			=> 0 %
+            -> PWM.duty_u16(16384) 	= 16384/65535 = 0,25	=> 25%
+            -> PWM.duty_u16(32768) 	= 32768/65535 = 0,5 	=> 50%
+            -> PWM.duty_u16(49152) 	= 49152/65535 = 0,75 	=> 75%
+            -> PWM.duty_u16(65535) 	= 65535/65535 = 1 		=> 100 %
+        """
     else :
         print("Etat inconnu")
         
-
-""" lecture du bouton
-val = BUTTON.value()
-if val == 1:
-    state = state + 1
-    if state > 4:
-        state = 0
-"""
